b118903d8574c43b980f26e8922955febac64f69
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-09-30 14:13:38 +0200

    Segundo Commit

--- src/LaOca.java
+++ src/LaOca.java
@@ -1,43 +1,62 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class LaOca {
-    private static class jugador implements Runnable{
+    private static class jugador implements Runnable {
         final int OBJETIVO = 50;
+
         @Override
         public void run() {
             Random r = new Random();
             int pos = 0;
-            while(pos != OBJETIVO){
-                try{
-                    Thread.sleep(r.nextLong(2000)+1);
-                    int dados = r.nextInt(6)+1;
+            while (pos != OBJETIVO) {
+                try {
+                    Thread.sleep(r.nextLong(2000) + 1);
+                    int dados = r.nextInt(6) + 1;
                     pos = dados + pos;
-                    if(pos<OBJETIVO){
-                        System.out.println("Locura el jugador saco: "+dados);
-                        System.out.println("Le faltan:"+(OBJETIVO-pos));
-                    } else if(pos>OBJETIVO){
+                    if (pos < OBJETIVO) {
+                        System.out.println("Locura el jugador saco: " + dados);
+                        System.out.println("Le faltan:" + (OBJETIVO - pos));
+                    } else if (pos > OBJETIVO) {
                         System.out.println("Tiene que volver atras");
-                        pos=OBJETIVO-(pos-OBJETIVO);
+                        pos = OBJETIVO - (pos - OBJETIVO);
+                    } else {
+                        System.out.println(Thread.currentThread().getName().toUpperCase() + " Llego al final");
                     }
 
-                }catch(InterruptedException e){
+                } catch (InterruptedException e) {
                     System.out.println("Tarea Interrumpida ");
                     break;
                 }
 
             }
-            System.out.println("Llego al final");
+
         }
     }
 
-    public static void main(String[] args) throws InterruptedException{
-        final int TAM = 4;
-        Thread[] t = new Thread[TAM];
-        for(int i =0;i==TAM;i++){
-            t[i]= new Thread(new jugador());
-        }
-        t.start();
-        t.join();
+    public static void main(String[] args) {
+        ArrayList<Thread> jugadores = new ArrayList<>();
+        jugadores.add(new Thread(new jugador()));
+        jugadores.add(new Thread(new jugador()));
+        jugadores.add(new Thread(new jugador()));
+        jugadores.add(new Thread(new jugador()));
 
+        boolean flag = true;
+
+        for (Thread a : jugadores) {
+            a.start();
+        }
+
+        while (flag) {
+            for (Thread i : jugadores) {
+                if (!i.isAlive()) {
+                    for (Thread e : jugadores) {
+                        e.interrupt();
+                    }
+                }
+                flag=false;
+            }
+
+        }
     }
 }
