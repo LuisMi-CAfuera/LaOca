c9351e0cd111f722888d74bfd684a875ba1f530a
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-04 09:46:04 +0200

    Commit sin comentarios Logger

--- src/LaOca.java
+++ src/LaOca.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class LaOca {
     private static class jugador implements Runnable {
-        final int OBJETIVO = 10;
+        //Esta Constante la utilizo para que cuando un jugador llegue a cien o se pase ponerla en la condicion y si tengo que cambiarle no tener que cambiar todo
+
+        final int OBJETIVO = 100;
 
         @Override
         public void run() {
@@ -35,10 +38,13 @@ public class LaOca {
     }
 
     public static void main(String[] args) throws InterruptedException{
+        Logger logging = PSPLogger.getLogger(LaOca.class.getName());
         final int TAM = 10;
         Thread[] t = new Thread[TAM];
         int i =0;
 
+
+        //Con este for lo que hacemos es inicializar todos los Threads a la vez que los metemos en el array
         for(i =0;i<TAM;i++){
             t[i]= new Thread(new jugador());
             t[i].start();
@@ -62,6 +68,8 @@ public class LaOca {
             }
             t[i].join();
         }
+
+        logging.info()
 
     }
 }
