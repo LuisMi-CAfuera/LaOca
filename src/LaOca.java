9a3f6c41bf3280f53a4838a4f40f5851c4c7b1cc
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-04 10:43:02 +0200

    Commit con comentarios Logger

--- src/LaOca.java
+++ src/LaOca.java
@@ -13,19 +13,21 @@ public class LaOca {
         public void run() {
             Random r = new Random();
             int pos = 0;
+
+
+
             while (pos != OBJETIVO) {
                 try {
                     Thread.sleep(r.nextLong(2000) + 1);
+                    //Aqui se lanzan los dados
                     int dados = r.nextInt(6) + 1;
                     pos = dados + pos;
-                    if (pos < OBJETIVO) {
+
                         logging.info("Locura el jugador " + Thread.currentThread().getName().toUpperCase() +" saco: " + dados);
                         logging.info("Le faltan:" + (OBJETIVO - pos));
-                    } else if (pos > OBJETIVO) {
-                        logging.info("Tiene que volver atras");
+                     if (pos > OBJETIVO) {
                         pos = OBJETIVO - (pos - OBJETIVO);
-                    } else {
-                        logging.info(Thread.currentThread().getName().toUpperCase() + " Llego al final");
+                         logging.info("Tiene que volver atras posicon "+ pos);
                     }
 
                 } catch (InterruptedException e) {
@@ -38,38 +40,42 @@ public class LaOca {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
         Logger logging = PSPLogger.getLogger(LaOca.class.getName());
         final int TAM = 10;
         Thread[] t = new Thread[TAM];
-        int i =0;
+        int i = 0;
 
 
         //Con este for lo que hacemos es inicializar todos los Threads a la vez que los metemos en el array
-        for(i =0;i<TAM;i++){
-            t[i]= new Thread(new jugador());
+        for (i = 0; i < TAM; i++) {
+            t[i] = new Thread(new jugador());
             t[i].start();
         }
         boolean flag = false;
-        int ganador =0;
+        int ganador = 0;
 
-        while(flag){
+            do {
             Thread.sleep(20);
-            for (i=0;i < TAM && !flag; i++){
-                if(!t[i].isAlive()){
+            for (i = 0; i < TAM && !flag; i++) {
+                if (!t[i].isAlive()) {
                     flag = true;
                     ganador = i;
+                    //aqui si no esta vivo directamente interrumpimos todos los jugadores
+
+                    for (i = 0; i < TAM; i++) {
+                        if (i != ganador) {
+                            t[i].interrupt();
+                        }
+                        t[i].join();
+                    }
+                    break;
                 }
             }
 
-        }
-        for (i=0;i<TAM;i++){
-            if(i!=ganador){
-                t[i].interrupt();
-            }
-            t[i].join();
-        }
+        } while(!flag);
 
+            //Aqui ponemos que es el Final y ponemos que se muestre el ganador
         logging.info("Final del programa");
         logging.info("Ha ganado el jugador" + t[ganador].getName());
 
